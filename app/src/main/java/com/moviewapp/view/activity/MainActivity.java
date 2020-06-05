d8d1286bd176b337ea2835372d51a8f71a14cb5a
nitin <nitin@ayopop.com>
2020-06-05 14:04:39 +0530

    package name changed

--- app/src/main/java/com/moviewapp/view/activity/MainActivity.java
+++ app/src/main/java/com/moviewapp/view/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.xebiademooapp;
+package com.moviewapp.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.xebiademooapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
